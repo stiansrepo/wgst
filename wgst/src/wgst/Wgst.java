c81bfe2f2d2fa62542281224e8b3670b343d3e39
stiansrepo <noises@gmail.com>
2015-04-21 17:08:56 +0200

    wg3

--- wgst/src/wgst/Wgst.java
+++ wgst/src/wgst/Wgst.java
@@ -2,24 +2,42 @@ package wgst;
 
 import java.awt.GridLayout;
 import javax.swing.*;
+import java.io.*;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 /**
  * @author DesktopStian
  */
-public class Wgst {
+public class Wgst extends JFrame{
 
-    public static void main(String[] args) {
+    public Wgst(){
+    
+        ImageIcon bg = new ImageIcon(getClass().getResource("/bgWhite.png"));
+        ImageIcon black = new ImageIcon(getClass().getResource("/black.png"));
     
         JFrame frame = new JFrame("Tile");
         JPanel panel = new JPanel(new GridLayout(10,10));
         JLabel[] map = new JLabel[100];
-        for(int i=0;i<100;i++){
-             map[i]=new JLabel();
+        
+        for(int i=0;i<100;i++){             
+             if(i%20==0){
+                 map[i]=new JLabel(black);
+             }
+             else{
+                 map[i]=new JLabel(bg);
+             }
              panel.add(map[i]);
         }        
         frame.add(panel);
-        frame.setVisible(true);
+        frame.pack();
+    }
         
+        public static void main(String[] args) throws Exception {
+            Wgst a = new Wgst();
+            a.setVisible(true);
+            a.pack();
     }
     
 }
